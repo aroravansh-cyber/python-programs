bd52f0aa307fbd7ef1584e83e63810b3a4319525
Vansh Arora <arora.vansh.00123@gmail.com>
2026-07-24 22:45:45 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,3 @@
+n=int(input("ENTER HOW MUCH LINE YOU WANT IN PATTERN"))
+for i in range(n,-n):
+    print("*"*i)
---
+++ rock paper.py
@@ -0,0 +1,10 @@
+
+rounds=int(input("""ENTER ROUNDS IN ODD NUMBER HOW MANY ROUNDS YOU WANT 
+        RULES: 1. Minimum 1 round you need to play
+               2. Maximum 13 rounds you need to play"""))
+while True:
+    ch=input("""ENTER YOUR CHOICE TO PLAY THE GAME
+    enter r for rock✋🏻
+    enter p for paper ✋🏻
+    enter s for scissor✌🏻""")
+    
