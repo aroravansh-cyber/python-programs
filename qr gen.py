ccb5ef21bda1d4942a83dcfa4169bf54fd266dfc
aroravansh00123-eng <arora.vansh.00123@gmail.com>
2026-08-01 23:43:35 +0530

    qr gen in python

---
+++ qr gen.py
@@ -0,0 +1,12 @@
+import qrcode
+url=input("Enter the URL: ")
+qr=qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=5
+)
+qr.add_data(url)
+qr.make(fit=True)
+img=qr.make_image(fill_color="black", back_color="white")
+img.save("qr_code.png")
+print("QR code generated and saved as qr_code.png")
